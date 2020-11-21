5d82e5a8b0c094d0fa9c780b7639ae28c4b7c8d6
Debasish Mukherjee <debasishformasters@gmail.com>
2020-11-21 17:49:26 +0100

    Demo template program
    No significance

---
+++ templates.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+template<typename T>
+class Container {
+    T _t;
+
+    public:
+    explicit Container (T t) : _t(t) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Container<T>& c) {
+        return(os << "The conatainer has : " << c._t);
+    }
+};
+
+int main () {
+
+Container<int> i(100);
+
+std::cout << i;
+
+return 0;
+}
