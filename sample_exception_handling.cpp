41dc484d9a1b42c332a73e1040e0b1dc1e718de0
Debasish Mukherjee <debasishformasters@gmail.com>
2020-11-25 16:31:07 +0100

    Exception example

--- sample_exception_handling.cpp
+++ sample_exception_handling.cpp
@@ -18,18 +18,23 @@ class customException : public std::exception {
 class somethingImportant {
     public:
     somethingImportant() {
+        #if 0
         throw customException("somethingImportant: Couldn't allocate important resource");
+        #endif
+        throw 10;
     }
 };
 
 int main () {
-
-try {
-    somethingImportant se;
-} catch (std::exception& e) {
-    //std::cout << "Caught exception of type: " << typeid(e).name() << std::endl;
-    std::cout << "Exception: " << e.what() << std::endl;
-    return 1;
-}
+    try {
+        somethingImportant se;
+    } catch (std::exception& e) {
+        //std::cout << "Caught exception of type: " << typeid(e).name() << std::endl;
+        std::cout << "Exception: " << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cout << "CatchAll Exception" << std::endl;
+        return 2;
+    }
 return 0;
 }
